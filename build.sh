3c9427e17f1ce7078d079b01919fbe6ec6ef8855
Neil Armstrong <superna9999@gmail.com>
2020-03-07 15:59:24 +0000

    Update run.sh, build.sh, .gitlab-ci.yml files

--- build.sh
+++ build.sh
@@ -3,14 +3,143 @@
 set -ex
 
 if [ "$#" -lt 3 ] ; then
-	echo "Usage: $0 <toolchain> <u-boot-path> <board>"
+	echo "Usage: $0 <toolchain> <u-boot-path> <board> <fipdir>"
 	exit 1
 fi
 
+TOOLCHAIN=$1
+UBOOT=$2
+BOARD=$3
+FIPDIR=$4
+
+# Build U-Boot
 (
-    cd $2
-    make O=build_$3 ${3}_defconfig > /dev/null
-    make O=build_$3 CROSS_COMPILE=$1/aarch64-linux-gnu- -j4 > /dev/null
+    cd $UBOOT
+    make O=build_$BOARD ${BOARD}_defconfig > /dev/null
+    make O=build_$BOARD CROSS_COMPILE=$TOOLCHAIN/aarch64-linux-gnu- -j4 > /dev/null
 )
+
+# Package with FIP
+DESTDIR=build_$BOARD/fip
+
+mkdir -p $DESTDIR
+
+case $BOARD in
+	"odroid-c2")
+	FIPDIR="$FIPDIR/odroid-c2"
+    SOC="odroid-c2"
+    ;;
+	"odroid-n2")
+	FIPDIR="$FIPDIR/odroid-n2"
+    SOC="g12b"
+    ;;
+	"p212")
+	FIPDIR="$FIPDIR/p212"
+    SOC="gxl"
+    ;;
+	"s400")
+	FIPDIR="$FIPDIR/s400"
+    SOC="axg"
+    ;;
+	"p201")
+	exit 0
+    ;;
+	"p200")
+	exit 0
+    ;;
+	"u200")
+    # TODO
+	exit 0
+    ;;
+	"libretech-cc")
+	FIPDIR="$FIPDIR/lepotato"
+    SOC="gxl"
+    ;;
+	"libretech-ac")
+	FIPDIR="$FIPDIR/lafrite"
+    SOC="gxl"
+    ;;
+	"khadas-vim")
+	FIPDIR="$FIPDIR/khadas-vim"
+    SOC="gxl"
+    ;;
+	"khadas-vim2")
+	FIPDIR="$FIPDIR/khadas-vim2"
+    SOC="gxm"
+    ;;
+	"khadas-vim3")
+	FIPDIR="$FIPDIR/khadas-vim3"
+    SOC="g12b"
+    ;;
+	"khadas-vim3l")
+	FIPDIR="$FIPDIR/khadas-vim3l"
+    SOC="sm1"
+    ;;
+	"nanopi-k2")
+	FIPDIR="$FIPDIR/nanopi-k2"
+    SOC="gxbb"
+    ;;
+	"sei510")
+    SOC="g12a"
+    # TODO
+	exit 0
+    ;;
+	"sei610")
+    SOC="sm1"
+    # TODO
+	exit 0
+    ;;
+    *)
+	echo "Unsupported board"
+	exit 1
+esac
+
+case $SOC in
+	"gxb" | "odroid-c2")
+    # TODO
+	exit 0
+	"gxl" | "gxm")
+    cp $FIPDIR/bl2.bin $DESTDIR/
+    cp $FIPDIR/acs.bin $DESTDIR/
+    cp $FIPDIR/bl21.bin $DESTDIR/
+    cp $FIPDIR/bl30.bin $DESTDIR/
+    cp $FIPDIR/bl301.bin $DESTDIR/
+    cp $FIPDIR/bl31.img $DESTDIR/
+    cp $UBOOT $DESTDIR/bl33.bin
+
+    $FIPDIR/blx_fix.sh $DESTDIR/bl30.bin \
+                       $DESTDIR/zero_tmp \
+                       $DESTDIR/bl30_zero.bin \
+                       $DESTDIR/bl301.bin \
+                       $DESTDIR/bl301_zero.bin \
+                       $DESTDIR/bl30_new.bin bl30
+
+    python3 $FIPDIR/acs_tool.py $DESTDIR/bl2.bin $DESTDIR/bl2_acs.bin $DESTDIR/acs.bin 0
+
+    $FIPDIR/blx_fix.sh $DESTDIR/bl2_acs.bin \
+                       $DESTDIR/zero_tmp \
+                       $DESTDIR/bl2_zero.bin \
+                       $DESTDIR/bl21.bin \
+                       $DESTDIR/bl21_zero.bin \
+                       $DESTDIR/bl2_new.bin bl2
+
+    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl30_new.bin
+    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl31.img
+    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl33.bin
+    $FIPDIR/aml_encrypt_gxl --bl2sig --input $DESTDIR/bl2_new.bin --output $DESTDIR/bl2.n.bin.sig
+    $FIPDIR/aml_encrypt_gxl --bootmk --output $DESTDIR/u-boot.bin --bl2 $DESTDIR/bl2.n.bin.sig --bl30 $DESTDIR/bl30_new.bin.enc --bl31 $DESTDIR/bl31.img.enc --bl33 $DESTDIR/bl33.bin.enc
+	;;
+	"axg")
+    # TODO
+	exit 0
+	;;
+	"g12a" | "sm1")
+    # TODO
+	exit 0
+	;;
+	"g12b")
+    # TODO
+	exit 0
+esac
 
 exit 0
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if [ "$#" -lt 6 ] ; then
-	echo "Usage: $0 <u-boot.bin> <board> <port> <tty> <prompt> <fipdir>"
+	echo "Usage: $0 <u-boot dir> <board> <port> <tty> <prompt>"
 	exit 1
 fi
 
@@ -12,47 +12,6 @@ BOARD=$2
 PORT=$3
 TTY=$4
 PROMPT=$5
-FIPDIR=$6
-DESTDIR=`mktemp -d`
-
-case $BOARD in
-	"libretech-cc")
-	FIPDIR="$FIPDIR/lepotato"
-    cp $FIPDIR/bl2.bin $DESTDIR/
-    cp $FIPDIR/acs.bin $DESTDIR/
-    cp $FIPDIR/bl21.bin $DESTDIR/
-    cp $FIPDIR/bl30.bin $DESTDIR/
-    cp $FIPDIR/bl301.bin $DESTDIR/
-    cp $FIPDIR/bl31.img $DESTDIR/
-    cp $UBOOT $DESTDIR/bl33.bin
-
-    $FIPDIR/blx_fix.sh $DESTDIR/bl30.bin \
-                       $DESTDIR/zero_tmp \
-                       $DESTDIR/bl30_zero.bin \
-                       $DESTDIR/bl301.bin \
-                       $DESTDIR/bl301_zero.bin \
-                       $DESTDIR/bl30_new.bin bl30
-
-    python3 $FIPDIR/acs_tool.py $DESTDIR/bl2.bin $DESTDIR/bl2_acs.bin $DESTDIR/acs.bin 0
-
-    $FIPDIR/blx_fix.sh $DESTDIR/bl2_acs.bin \
-                       $DESTDIR/zero_tmp \
-                       $DESTDIR/bl2_zero.bin \
-                       $DESTDIR/bl21.bin \
-                       $DESTDIR/bl21_zero.bin \
-                       $DESTDIR/bl2_new.bin bl2
-
-    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl30_new.bin
-    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl31.img
-    $FIPDIR/aml_encrypt_gxl --bl3enc --input $DESTDIR/bl33.bin
-    $FIPDIR/aml_encrypt_gxl --bl2sig --input $DESTDIR/bl2_new.bin --output $DESTDIR/bl2.n.bin.sig
-    $FIPDIR/aml_encrypt_gxl --bootmk --output $DESTDIR/u-boot.bin --bl2 $DESTDIR/bl2.n.bin.sig --bl30 $DESTDIR/bl30_new.bin.enc --bl31 $DESTDIR/bl31.img.enc --bl33 $DESTDIR/bl33.bin.enc
-	;;
-
-	*)
-	echo "Unsupported board"
-	exit 1
-esac
 
 stty -F /dev/$TTY 115200 cs8 parenb
 
@@ -61,7 +20,7 @@ sleep 1
 sh /opt/lab-tools/amaz.sh -a on -p $PORT
 sleep 1
 
-/opt/pyamlboot/boot.py --board-files $DESTDIR/ $BOARD
+/opt/pyamlboot/boot.py --board-files $UBOOT $BOARD
 
 N=0
 while read -ra bytes
