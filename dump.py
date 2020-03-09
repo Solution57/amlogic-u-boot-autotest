ed7e841d4386c11eac3523c64a44d37ea41511ec
Neil Armstrong <superna9999@gmail.com>
2020-03-09 13:04:26 +0000

    Update run.sh, .gitlab-ci.yml, dump.py files

---
+++ dump.py
@@ -0,0 +1,19 @@
+import sys
+import serial
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        PORT = sys.argv[1]
+    if len(sys.argv) > 2:
+        PROMPT = sys.argv[2]
+
+    s = serial.serial_for_url(PORT, 115200)
+    stop = False
+
+    while not stop:
+        line = s.readline()
+        if PROMPT in line:
+            stop = True
+        sys.stdout.write("> %s" % line)
+
+    s.close()
--- run.sh
+++ run.sh
@@ -13,24 +13,15 @@ PORT=$3
 TTY=$4
 PROMPT=$5
 
-stty -F /dev/$TTY 115200 cs8 parenb
-
 sh /opt/lab-tools/amaz.sh -a off -p $PORT
 sleep 1
+
 sh /opt/lab-tools/amaz.sh -a on -p $PORT
 sleep 1
 
 /opt/pyamlboot/boot.py --board-files $UBOOT $BOARD
 
-N=0
-while read -ra bytes
-do
-  echo "$N> $bytes"
-  N=`expr $N + 1`
-  if echo $bytes | grep $PROMPT ; then
-	break
-  fi
-done < /dev/$TTY
+python3 dump.py /dev/$TTY "$PROMPT"
 
 sh /opt/lab-tools/amaz.sh -a off -p $PORT
 
